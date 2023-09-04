f9b4458c839360fc51a75831f59756c92d4569d3
Lesykhaievskiy <lesukhaievskiy@gmail.com>
2023-09-04 23:10:56 +0300

    Update main text

--- TodoList/ListRowView.swift
+++ TodoList/ListRowView.swift
@@ -23,8 +23,8 @@ struct ListRowView: View {
 }
 
 struct ListRowView_Previews: PreviewProvider {
-    static var item1 = itemModel(title: "This is the first item", isCompleted: false)
-    static var item2 = itemModel(title: "This is the second item", isCompleted: true)
+    static var item1 = itemModel(title: "This is the 1st item", isCompleted: false)
+    static var item2 = itemModel(title: "This is the 2nd item", isCompleted: true)
     static var previews: some View {
         Group{
             ListRowView(item: item1)
